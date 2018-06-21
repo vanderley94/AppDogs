68c919265b13b2fa996aa7db704591abdecc7a76
vanderley94 <vanderleyquaresma@live.com.pt>
2018-06-21 14:51:18 +0100

    Added Raças database seed

--- app/src/main/java/pt/ipg/appdogs/BdAppDogsOpenHelper.java
+++ app/src/main/java/pt/ipg/appdogs/BdAppDogsOpenHelper.java
@@ -6,6 +6,8 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class BdAppDogsOpenHelper extends SQLiteOpenHelper {
 
+    private static final boolean PRODUCTION = false;
+
     public static final String DOGS_DB = "Dogs.db";
     private static final int BASEDADOS_VERSION = 1;
 
@@ -25,6 +27,58 @@ public class BdAppDogsOpenHelper extends SQLiteOpenHelper {
 
         BdTabelaTratamento bdTabelaTratamento = new BdTabelaTratamento(db);
         bdTabelaTratamento.criar();
+
+        if(!PRODUCTION){
+            seed(db);
+        }
+
+    }
+
+    private void seed(SQLiteDatabase db){
+        BdTabelaRaca bdTabelaRaca = new BdTabelaRaca(db);
+
+        Raca raca = new Raca();
+        raca.setNome("Pitbul");
+        int idRacaPitbul = (int) bdTabelaRaca.insert(BdTabelaRaca.getContentValues(raca));
+
+
+        raca = new Raca();
+        raca.setNome("Bulldog");
+        int idRacaBulldog = (int) bdTabelaRaca.insert(BdTabelaRaca.getContentValues(raca));
+
+        raca = new Raca();
+        raca.setNome("Labrador");
+        int idRacaLabrador = (int) bdTabelaRaca.insert(BdTabelaRaca.getContentValues(raca));
+
+
+        BdTabelaAcidente bdTabelaAcidente = new BdTabelaAcidente(db);
+
+        Acidente acidente = new Acidente();
+        acidente.setTipoDeAcidente("Lesão nas patas");
+        acidente.getIdCao(idRacaPitbul);
+        bdTabelaAcidente.insert(bdTabelaAcidente.getContentValues(acidente));
+
+        int idAcidente =  (int) bdTabelaAcidente.insert(BdTabelaAcidente.getContentValues(acidente));
+
+        acidente = new Acidente();
+        acidente.setTipoDeAcidente("Lesão na parte de tronco");
+        acidente.getIdCao(idRacaBulldog);
+        bdTabelaAcidente.insert(bdTabelaAcidente.getContentValues(acidente));
+
+        acidente = new Acidente();
+        acidente.setTipoDeAcidente("Lesão na cabeça");
+        acidente.getIdCao(idRacaLabrador);
+        bdTabelaAcidente.insert(bdTabelaAcidente.getContentValues(acidente));
+        
+
+        BdTabelaTratamento bdTabelaTratamento = new BdTabelaTratamento(db);
+
+        Tratamento tratamento = new Tratamento();
+        tratamento.setDescricao("Esticar a pena e dirigir ao centro de tramaneto!");
+        tratamento.getIDAcidente(idAcidente);
+
+
+
 
     }
 
